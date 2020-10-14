e01042ea9787a1b403feeae838e9f05c9c0a810c
brenu <bvsousa64@gmail.com>
2020-10-13 23:06:13 -0300

    :rocket: Make minor changes in queries and add route for lesson post

--- api/teacher/clas.js
+++ api/teacher/clas.js
@@ -1,71 +1,97 @@
-const knex = require('../../config/db')
+const knex = require("../../config/db");
 
-module.exports = app => {
-    const { existsOrError } = app.api.validator
-    
-    const getClas = async (req, res) => {
-        try {
-            existsOrError(req.params.id, 'clas does not exist!')
-            
-            employee_id = req.params.id;
+module.exports = (app) => {
+  const { existsOrError } = app.api.validator;
 
-            const getIdSelectClas = await knex('employee')
-            .select("person.person_name", "classroom.classroom_name")
-            .innerJoin("clas", "employee.employee_id", "clas.employee_id")
-            .innerJoin("student", "student.student_id", "clas.student_id")
-            .innerJoin("classroom", "classroom.classroom_id", "student.classroom_id")
-            .innerJoin("person", "person.person_id", "student.person_id")
-                .where( "employee.employee_id", req.params.id )
-            existsOrError(getIdSelectClas, 'clas not found')
+  const getClas = async (req, res) => {
+    try {
+      existsOrError(req.params.id, "clas does not exist!");
 
-            res.json(getIdSelectClas)
-        } catch (msg) {
-            return res.status(400).send(msg)
-        }
+      employee_id = req.params.id;
+
+      const getIdSelectClas = await knex("employee")
+        .select(
+          "person.person_name",
+          "classroom.classroom_name",
+          "discipline.discipline_id",
+          "discipline.discipline_name"
+        )
+        .innerJoin("clas", "employee.employee_id", "clas.employee_id")
+        .innerJoin("student", "student.student_id", "clas.student_id")
+        .innerJoin(
+          "discipline",
+          "discipline.discipline_id",
+          "clas.discipline_id"
+        )
+        .innerJoin(
+          "classroom",
+          "classroom.classroom_id",
+          "student.classroom_id"
+        )
+        .innerJoin("person", "person.person_id", "student.person_id")
+        .where("employee.employee_id", req.params.id);
+      existsOrError(getIdSelectClas, "clas not found");
+
+      res.json(getIdSelectClas);
+    } catch (msg) {
+      return res.status(400).send(msg);
     }
+  };
 
-    const getDiscipline = async (req, res) => {
-        try {
-            existsOrError(req.params.id, 'clas does not exist!')
-    
-            discipline_id = req.params.id
+  const getDiscipline = async (req, res) => {
+    try {
+      existsOrError(req.params.id, "clas does not exist!");
 
-            const getIdSelectClas = await knex('employee')
-            .select("person.person_name", "classroom.classroom_name")
-            .innerJoin("clas", "employee.employee_id", "clas.employee_id")
-            .innerJoin("student", "student.student_id", "clas.student_id")
-            .innerJoin("classroom", "classroom.classroom_id", "student.classroom_id")
-            .innerJoin("person", "person.person_id", "student.person_id")
-                .where( "employee.employee_id", employee_id )
-                .where( "clas.discipline_id", req.params.id )
-            existsOrError(getIdSelectClas, 'clas not found')
+      discipline_id = req.params.id;
 
-            res.json(getIdSelectClas)
-        } catch (msg) {
-            return res.status(400).send(msg)
-        }
+      const getIdSelectClas = await knex("employee")
+        .select(
+          "person.person_name",
+          "classroom.classroom_name",
+          "classroom.classroom_id"
+        )
+        .innerJoin("clas", "employee.employee_id", "clas.employee_id")
+        .innerJoin("student", "student.student_id", "clas.student_id")
+        .innerJoin(
+          "classroom",
+          "classroom.classroom_id",
+          "student.classroom_id"
+        )
+        .innerJoin("person", "person.person_id", "student.person_id")
+        .where("employee.employee_id", employee_id)
+        .where("clas.discipline_id", req.params.id);
+      existsOrError(getIdSelectClas, "clas not found");
+
+      res.json(getIdSelectClas);
+    } catch (msg) {
+      return res.status(400).send(msg);
     }
+  };
 
-    const getClassroom = async (req, res) => {
-        try {
-            existsOrError(req.params.id, 'clas does not exist!')
+  const getClassroom = async (req, res) => {
+    try {
+      existsOrError(req.params.id, "clas does not exist!");
 
-            const getIdSelectClas = await knex('employee')
-            .select("person.person_name", "classroom.classroom_name")
-            .innerJoin("clas", "employee.employee_id", "clas.employee_id")
-            .innerJoin("student", "student.student_id", "clas.student_id")
-            .innerJoin("classroom", "classroom.classroom_id", "student.classroom_id")
-            .innerJoin("person", "person.person_id", "student.person_id")
-                .where( "employee.employee_id", employee_id )
-                .where( "clas.discipline_id", discipline_id )
-                .where( "student.classroom_id", req.params.id )
-            existsOrError(getIdSelectClas, 'clas not found')
+      const getIdSelectClas = await knex("employee")
+        .select("person.person_name", "classroom.classroom_name")
+        .innerJoin("clas", "employee.employee_id", "clas.employee_id")
+        .innerJoin("student", "student.student_id", "clas.student_id")
+        .innerJoin(
+          "classroom",
+          "classroom.classroom_id",
+          "student.classroom_id"
+        )
+        .innerJoin("person", "person.person_id", "student.person_id")
+        .where("employee.employee_id", employee_id)
+        .where("clas.discipline_id", discipline_id)
+        .where("student.classroom_id", req.params.id);
+      existsOrError(getIdSelectClas, "clas not found");
 
-            res.json(getIdSelectClas)
-        } catch (msg) {
-            return res.status(400).send(msg)
-        }
+      res.json(getIdSelectClas);
+    } catch (msg) {
+      return res.status(400).send(msg);
     }
- 
-    return { getClas, getDiscipline, getClassroom}
-}
+  };
+
+  return { getClas, getDiscipline, getClassroom };
+};
